da78e2d8fa8b2c8172c63013888b1ee50fe919f5
Newteq <newt.fourie@entelect.co.za>
2019-07-03 15:43:58 +0200

    first usage of the wcf service and finding that there are exceptions that need to be caught

--- CoreWebApi/Controllers/WcfController.cs
+++ CoreWebApi/Controllers/WcfController.cs
@@ -1,15 +1,54 @@
 ﻿using CoreWebApi.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
+using SampleWcfService;
+using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
 
 namespace CoreWebApi.Controllers
 {
     public class WcfController : ApiBaseController
     {
         [HttpGet]
-        [Route("echo")]
-        public IActionResult Echo()
+        [Route("hello")]
+        public IActionResult HelloWorld()
         {
             return Ok("hello world");
+        }
+
+        [HttpGet]
+        [Route("echo")]
+        public async Task<IActionResult> Echo(string input)
+        {
+            var wcfClient = new WcfServiceClient();
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            {
+                input = "nothing set";
+            }
+
+            var output = "An error has occured \r\n";
+
+            try
+            {
+                output = await wcfClient.EchoAsync(input);
+            }
+            catch (CommunicationException commsException)
+            {
+                #if DEBUG
+                output += $"The service is unreachable. The error from the exception is: {commsException.Message}";
+                #else
+                output += $"The service is unreachable.";
+                #endif
+            }
+            catch(Exception e)
+            {
+                #if DEBUG
+                output += $"Some unexpected exception occured. The error message is: {e.Message}\r\n\r\n The stacktrace is:\r\n{e.StackTrace}";
+                #else
+                output += $"Some unexpected exception occured.";
+                #endif
+            }
+            return Ok(output);
         }
     }
 }
