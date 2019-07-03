6161b9881448eeceb1d5ec7c917da08b81e96863
Newteq <newt.fourie@entelect.co.za>
2019-07-03 15:47:52 +0200

    Using hosting environment instead of pragma's cause I hate that shit

--- CoreWebApi/Controllers/WcfController.cs
+++ CoreWebApi/Controllers/WcfController.cs
@@ -1,4 +1,5 @@
 ﻿using CoreWebApi.Controllers.Base;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using SampleWcfService;
 using System;
@@ -9,6 +10,13 @@ namespace CoreWebApi.Controllers
 {
     public class WcfController : ApiBaseController
     {
+        public IHostingEnvironment Environment { get; }
+
+        public WcfController(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
         [HttpGet]
         [Route("hello")]
         public IActionResult HelloWorld()
@@ -34,19 +42,25 @@ namespace CoreWebApi.Controllers
             }
             catch (CommunicationException commsException)
             {
-                #if DEBUG
-                output += $"The service is unreachable. The error from the exception is: {commsException.Message}";
-                #else
-                output += $"The service is unreachable.";
-                #endif
+                if (Environment.IsDevelopment())
+                {
+                    output += $"The service is unreachable. The error from the exception is: {commsException.Message}";
+                }
+                else
+                {
+                    output += $"The service is unreachable.";
+                }
             }
             catch(Exception e)
             {
-                #if DEBUG
-                output += $"Some unexpected exception occured. The error message is: {e.Message}\r\n\r\n The stacktrace is:\r\n{e.StackTrace}";
-                #else
-                output += $"Some unexpected exception occured.";
-                #endif
+                if (Environment.IsDevelopment())
+                {
+                    output += $"Some unexpected exception occured. The error message is: {e.Message}\r\n\r\n The stacktrace is:\r\n{e.StackTrace}";
+                }
+                else
+                {
+                    output += $"Some unexpected exception occured.";
+                }
             }
             return Ok(output);
         }
