624c251b06d81d60d2317659b45220b1d9cace79
Newteq <newt.fourie@entelect.co.za>
2019-07-03 12:03:42 +0200

    Make a new test controller

---
+++ CoreWebApi/Controllers/Base/ApiBaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApi.Controllers.Base
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+    }
+}
---
+++ CoreWebApi/Controllers/WcfController.cs
@@ -0,0 +1,14 @@
+﻿using CoreWebApi.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi.Controllers
+{
+    public class WcfController : ApiBaseController
+    {
+        [HttpGet]
+        public IActionResult EchoWcf()
+        {
+            return Ok("hello world");
+        }
+    }
+}
