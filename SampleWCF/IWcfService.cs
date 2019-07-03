1d944bf27f722d3fef135f8824e05465954ebc15
Newteq <newt.fourie@entelect.co.za>
2019-07-03 11:54:56 +0200

    Create new wcf service method

--- SampleWCF/IWcfService.cs
+++ SampleWCF/IWcfService.cs
@@ -10,7 +10,7 @@ namespace SampleWCF
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IWcfService
     {
 
         [OperationContract]
@@ -19,7 +19,8 @@ namespace SampleWCF
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
-        // TODO: Add your service operations here
+        [OperationContract]
+        string Echo(string echoWord);
     }
 
 
--- SampleWCF/WcfService.svc.cs
+++ SampleWCF/WcfService.svc.cs
@@ -8,9 +8,7 @@ using System.Text;
 
 namespace SampleWCF
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class WcfService : IWcfService
     {
         public string GetData(int value)
         {
@@ -28,6 +26,11 @@ namespace SampleWCF
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+        public string Echo(string echoWord)
+        {
+            return $"The word you said with GET is: {echoWord}";
         }
     }
 }
